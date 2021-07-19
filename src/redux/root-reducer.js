ef3c62cb59ec771f68602d31f7048b9b3c4cf888
Vyacheslav Hludnev <62879099+vhludnev@users.noreply.github.com>
2021-07-19 13:14:06 +0300

    localStorage for cart page inplementation with redux persist

--- src/redux/root-reducer.js
+++ src/redux/root-reducer.js
@@ -1,9 +1,48 @@
+// Neither Local nor Sesson Storage integrated: //
+// import { combineReducers } from 'redux';
+
+// import userReducer from './user/user.reducer';
+// import cartReducer from './cart/cart.reducer';
+
+// export default combineReducers({ 
+// 	user: userReducer, 
+// 	cart: cartReducer 
+// });
+
+
+// LocalStorage implementation: //
 import { combineReducers } from 'redux';
+import { persistReducer } from 'redux-persist';
+import storage from 'redux-persist/lib/storage'; // makes localStorage become default storage
 
 import userReducer from './user/user.reducer';
 import cartReducer from './cart/cart.reducer';
 
-export default combineReducers({ 
-	user: userReducer, 
-	cart: cartReducer 
+const persistConfig = { key: 'root', storage, whitelist: ['cart'] };
+
+const rootReducer = combineReducers({
+	user: userReducer,
+	cart: cartReducer
 });
+
+export default persistReducer(persistConfig, rootReducer);
+
+// SessionStorage implementation: //
+// import { combineReducers } from 'redux';
+// import { persistReducer } from 'redux-persist';
+// import storage from 'redux-persist/lib/storage'; // makes localStorage become default storage
+// import storageSession from 'redux-persist/lib/storage/session' // the same as above but for Session storage
+
+// import userReducer from './user/user.reducer';
+// import cartReducer from './cart/cart.reducer';
+
+// const persistConfig = { key: 'root', storage, whitelist: ['cart'] };
+// const cartPersistConfig = { key: 'root', storage:storageSession };
+
+// const rootReducer = combineReducers({
+// 	user: userReducer,
+// 	//cart: cartReducer
+// 	cart: persistReducer(cartPersistConfig, cartReducer)
+// });
+
+// export default persistReducer(persistConfig, rootReducer);
--- src/redux/store.js
+++ src/redux/store.js
@@ -1,11 +1,14 @@
 import { createStore, applyMiddleware } from 'redux';
 import logger from 'redux-logger'; // catches actions and console.logs them (good for debugging)
+import { persistStore } from 'redux-persist'; // lets the browser cash the store
 
 import rootReducer from './root-reducer';
 
 const middlewares = [logger]; // an Array of "console.logs"
 
 // const store = createStore(rootReducer, applyMiddleware(logger));  --> when there is only one middleware in project
-const store = createStore(rootReducer, applyMiddleware(...middlewares)); // "applyMiddleware(...middlewares)" spreads combined middleware into single logs
+export const store = createStore(rootReducer, applyMiddleware(...middlewares)); // "applyMiddleware(...middlewares)" spreads combined middleware into single logs
 
-export default store;
+export const persistor = persistStore(store);
+
+// export default { store, persistor }; // not necessary, since already exporting them above
