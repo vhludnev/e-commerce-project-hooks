63f3549166ba45d9ad22ccd2005d86533b980b36
Vyacheslav Hludnev <62879099+vhludnev@users.noreply.github.com>
2021-08-15 19:23:36 +0300

    rearranged directory categories and improved dropdown menu styles

--- src/contexts/directory/directory.data.js
+++ src/contexts/directory/directory.data.js
@@ -1,14 +1,28 @@
 const DIRECTORY_DATA = [
 	{
+		title: 'womens',
+		imageUrl: 'https://i.ibb.co/GCCdy8t/womens.png',
+		size: 'large',
+		id: 1,
+		linkUrl: 'shop/womens'
+	},
+	{
+		title: 'mens',
+		imageUrl: 'https://i.ibb.co/R70vBrQ/men.png',
+		size: 'large',
+		id: 2,
+		linkUrl: 'shop/mens'
+	},
+	{
 		title: 'hats',
 		imageUrl: 'https://i.ibb.co/cvpntL1/hats.png',
-		id: 1,
+		id: 4,
 		linkUrl: 'shop/hats'
 	},
 	{
 		title: 'jackets',
 		imageUrl: 'https://i.ibb.co/px2tCc3/jackets.png',
-		id: 2,
+		id: 5,
 		linkUrl: 'shop/jackets'
 	},
 	{
@@ -16,20 +30,6 @@ const DIRECTORY_DATA = [
 		imageUrl: 'https://i.ibb.co/0jqHpnp/sneakers.png',
 		id: 3,
 		linkUrl: 'shop/sneakers'
-	},
-	{
-		title: 'womens',
-		imageUrl: 'https://i.ibb.co/GCCdy8t/womens.png',
-		size: 'large',
-		id: 4,
-		linkUrl: 'shop/womens'
-	},
-	{
-		title: 'mens',
-		imageUrl: 'https://i.ibb.co/R70vBrQ/men.png',
-		size: 'large',
-		id: 5,
-		linkUrl: 'shop/mens'
 	}
 ];
   
